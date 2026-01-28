567d08445e536139cfb4ab1becb72861a0f6919e
KayDag <dangmaikhanhkn@gmail.com>
2026-01-28 18:08:20 +0700

    Add: Save Game

---
+++ Assets/KeyData/KeyDataPlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyDataPlayer
+{
+    public static string gold = "Gold";
+    public static string wave = "Wave";
+    public static string bomber = "Bomber";
+    public static string scout = "Scout";
+    public static string checkPoint = "CheckPoint";
+    public static string statsScout = "Stats Scout";
+    public static string statsBomber = "Stats Bomber";
+    public static string statsCastle = "Stats Castle";
+    public static string statsBooster = "Stats Booster";
+}
